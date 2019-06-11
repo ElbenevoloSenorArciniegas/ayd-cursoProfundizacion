9902370827f5e1c6fe270e9b4f8ffe7e75830d89
El caballero de la noche <fredy_yamid@hotmail.com>
2019-06-10 22:32:37 -0500

    parcheSQL

--- cursoprofundizacion.sql
+++ cursoprofundizacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2019 a las 05:26:28
+-- Tiempo de generación: 11-06-2019 a las 05:31:46
 -- Versión del servidor: 10.1.30-MariaDB
 -- Versión de PHP: 7.2.2
 
